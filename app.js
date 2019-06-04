df98a4e16bf2741277911937f572fd4b3d66f985
priaajisn <priaaji.setyanegoro@gmail.com>
2019-06-05 04:50:44 +0700

    upload file project

---
+++ app.js
@@ -0,0 +1,31 @@
+const btn = document.querySelector(".talk");
+const content = document.querySelector(".content");
+
+const SpeechRecognition = window.SpeechRecognition || window.webkitSpeechRecognition;
+const recognition = new SpeechRecognition();
+
+recognition.onstart = function() {
+    console.log("voice is actived , you can to microphone");
+}
+
+recognition.onresult = function (event) {
+    const current = event.resultIndex;
+    const transcript = event.results[current][0].transcript;
+    content.textContent = transcript;
+    readOutLoud(transcript);
+    console.log(event);
+}
+
+btn.addEventListener("click" , () => {
+    recognition.start();
+});
+
+function readOutLoud(m){
+    const speech = new SpeechSynthesisUtterance();
+    speech.text = m;
+    speech.volume = 1;
+    speech.rate = 1;
+    speech.pitch = 1;
+
+    window.speechSynthesis.speak(speech);
+}
